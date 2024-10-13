e977a5ec68608e8d62ff4aec7c8df1f3b3da6ce3
Gabriel <gabrieldantaslp@gmail.com>
2024-10-13 11:57:44 -0300

    chore: Angular Material setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: "app.component.html",
 })
 export class AppComponent {
